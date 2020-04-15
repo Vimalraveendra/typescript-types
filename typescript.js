315d60abcef813eb94a4921b56e8db2bced7f576
vimal Raveendran <vimalraveendra@gmail.com>
2020-04-15 11:31:02 +0200

    initial commit

---
+++ typescript.js
@@ -0,0 +1,21 @@
+//boolean
+var isCool = true;
+// number
+var age = 43;
+// string
+// this works with ES6 template string as well
+var eyeColor = "brown";
+var favouriteQuote = "I 'm not old . I 'm only " + age;
+//Arrays
+// there are two ways we can define the arrays
+var pets = ["cats", "dogs", "rabbits"];
+var cars = ["benz", "bmw", "ford"];
+// objects
+// here you noticed that it's lowercase object type because
+// uppercase Object  you can use in javascript.
+var wizard = {
+    a: "john"
+};
+// null and undefined
+var meh = undefined;
+var noo = null;
---
+++ typescript.ts
@@ -0,0 +1,29 @@
+//boolean
+
+let isCool: boolean = true;
+
+// number
+
+let age: number = 43;
+
+// string
+// this works with ES6 template string as well
+let eyeColor: string = "brown";
+let favouriteQuote: string = `I 'm not old . I 'm only ${age}`;
+
+//Arrays
+// there are two ways we can define the arrays
+let pets: string[] = ["cats", "dogs", "rabbits"];
+let cars: Array<string> = ["benz", "bmw", "ford"];
+
+// objects
+// here you noticed that it's lowercase object type because
+// uppercase Object  you can use in javascript.
+let wizard: object = {
+  a: "john"
+};
+
+// null and undefined
+
+let meh: undefined = undefined;
+let noo: null = null;
