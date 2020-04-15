15747ec9cfcdd0e697c6730d9c1235650750243a
vimal Raveendran <vimalraveendra@gmail.com>
2020-04-15 17:37:04 +0200

    updated the typescript with static types

--- typescript.js
+++ typescript.js
@@ -19,3 +19,54 @@ var wizard = {
 // null and undefined
 var meh = undefined;
 var noo = null;
+// Tuple
+// with tuple you can have different types inside of it
+var basket;
+basket = ["basketball", 5];
+//enum
+var Size;
+(function (Size) {
+    Size[Size["small"] = 1] = "small";
+    Size[Size["medium"] = 2] = "medium";
+    Size[Size["large"] = 3] = "large";
+})(Size || (Size = {}));
+var sizeName = Size[2];
+var sizeName1 = Size.small;
+//Any  -!!!!!!!!!!  Be Careful
+// whatever can be whatever we want
+//Any type as the name suggest can be any type that we want.
+//Any type neglects  all the benefits that typescript would give you
+// because  whatever will never throw out an error it can be whatever
+// we want.
+var whatever = "aghhhhhhhh nooooooo";
+whatever = basket;
+whatever = 10;
+//void
+//function that return nothing is a void
+var sing = function () {
+    console.log("lalalalalaallallallalal");
+};
+//never
+//Never types actually test two things
+//one is that function that never returns
+// and other one  is that variable  under some sort fo type is
+// never true means a not a reachable end point.always throw an error
+//The never type is used for functions which never return a value.
+//This means, a function which only throws an error, returns never a value.
+//Even not void.
+var error = function () {
+    throw Error("Oooops");
+};
+var fightRobotArmy = function (robots) {
+    // this interface RobotArmy is going to make sure that the
+    // robots parmater that i'm recieving is going to have  count
+    // that has type of number , name of type strin & magic of type string.
+    // otherwise  its kind of throw an error. this might be useful for react & props
+    console.log("fight");
+};
+var fightRobotArmy1 = function (robots) {
+    console.log("fight1");
+};
+var mostFavouritePets = function (pets) {
+    console.log("petssss");
+};
--- typescript.ts
+++ typescript.ts
@@ -27,3 +27,85 @@ let wizard: object = {
 
 let meh: undefined = undefined;
 let noo: null = null;
+
+// Tuple
+// with tuple you can have different types inside of it
+let basket: [string, number];
+basket = ["basketball", 5];
+
+//enum
+enum Size {
+  small = 1,
+  medium = 2,
+  large = 3
+}
+let sizeName: string = Size[2];
+let sizeName1: number = Size.small;
+
+//Any  -!!!!!!!!!!  Be Careful
+// whatever can be whatever we want
+//Any type as the name suggest can be any type that we want.
+//Any type neglects  all the benefits that typescript would give you
+// because  whatever will never throw out an error it can be whatever
+// we want.
+let whatever: any = "aghhhhhhhh nooooooo";
+whatever = basket;
+whatever = 10;
+
+//void
+//function that return nothing is a void
+
+let sing = (): void => {
+  console.log("lalalalalaallallallalal");
+};
+
+//never
+//Never types actually test two things
+//one is that function that never returns
+// and other one  is that variable  under some sort fo type is
+// never true means a not a reachable end point.always throw an error
+//The never type is used for functions which never return a value.
+//This means, a function which only throws an error, returns never a value.
+//Even not void.
+let error = (): never => {
+  throw Error("Oooops");
+};
+
+// interface
+// interface can be used really really well with objects
+// we created a interface RobotArmy thats says that from now
+//on  this RobotArmy can act as a type that has these parmaters
+interface RobotArmy {
+  count: number;
+  name: string;
+  magic: string;
+}
+
+let fightRobotArmy = (robots: RobotArmy) => {
+  // this interface RobotArmy is going to make sure that the
+  // robots parmater that i'm recieving is going to have  count
+  // that has type of number , name of type strin & magic of type string.
+  // otherwise  its kind of throw an error. this might be useful for react & props
+  console.log("fight");
+};
+
+let fightRobotArmy1 = (robots: {
+  count: number;
+  name: string;
+  magic: string;
+}) => {
+  console.log("fight1");
+};
+
+// type
+// type can be used really really well with objects
+// we created a type FavouritePets thats says that from now
+//on  this FavouritePet can act as a type that has these parmaters
+type FavouritePet = {
+  id: number;
+  name: string;
+};
+
+let mostFavouritePets = (pets: FavouritePet) => {
+  console.log("petssss");
+};
